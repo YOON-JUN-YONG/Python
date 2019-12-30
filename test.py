d8f80714b3af9a4fc0e6a46c3abc528f764ae52e
YOON <wnsdyd1124@gmail.com>
2019-12-30 21:57:08 +0900

    계산기

--- test.py
+++ test.py
@@ -9,9 +9,44 @@ def mul(a,b):
 def div(a,b):
     return a / b
 
-input("Calculator machine")
+print("Calculator machine")
+print("If you press 'Enter', You can exit")
 
-Cal = str(input())
+while True:
+
+    Cal = str(input())
+
+    if Cal[1] == "+":
+        a = int(Cal[0])
+        b = int(Cal[2])
+        result = add(a,b)
+        print(result)
+
+    if Cal[1] == "-":
+        a = int(Cal[0])
+        b = int(Cal[2])
+        result = sub(a,b)
+        print(result)
+
+    if Cal[1] == "*":
+        a = int(Cal[0])
+        b = int(Cal[2])
+        result = mul(a,b)
+        print(result)
+
+    if Cal[1] == "/":
+        a = int(Cal[0])
+        b = int(Cal[2])
+        result = div(a,b)
+        print(result)
+
+    if Cal[0] == "":
+        sys.exit()
+
+    if Cal[1] != "+" or "-" or "*" or "/":
+        print("please reenter")
+
+
 
 
 
